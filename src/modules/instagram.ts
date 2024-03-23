09a85fd43a566efa3082065a3f251f819d823196
Amir Ziaei <amir.ziaei@zenitech.co.uk>
2024-03-24 01:26:40 +0200

    less flaky

--- src/modules/instagram.ts
+++ src/modules/instagram.ts
@@ -12,17 +12,32 @@ export async function sendDirectToThread({
   threadId,
   authStatePath,
 }: SendDirectToThreadParams) {
-  const browser = await chromium.launch({ headless: true })
+  const browser = await chromium.launch({ headless: false })
   console.log('sendDirectToThread: Chromium launched')
-  const context = await browser.newContext({ storageState: authStatePath })
+  const context = await browser.newContext({
+    storageState: authStatePath,
+    locale: 'en-US',
+    timezoneId: 'Europe/Vilnius',
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+  })
+  console.log('sendDirectToThread: Context created')
   const page = await context.newPage()
+  console.log('sendDirectToThread: New page opened')
   const threadUrl = `https://www.instagram.com/direct/t/${threadId}/`
   await page.goto(threadUrl)
+  console.log('sendDirectToThread: Went to thread url')
   const pageUrl = page.url()
   console.log(`sendDirectToThread: Instagram launched:: ${pageUrl}`)
   const isAuthenticated = pageUrl === threadUrl
   console.log(`sendDirectToThread: Authentication status: ${isAuthenticated}`)
   if (!isAuthenticated) {
+    try {
+      await page
+        .getByRole('button', { name: `Continue as ${process.env.IG_USERNAME}` })
+        .click()
+      console.log(`sendDirectToThread: Continued as ${process.env.IG_USERNAME}`)
+    } catch {}
     try {
       await page
         .getByRole('button', { name: 'Decline optional cookies' })
@@ -46,14 +61,21 @@ export async function sendDirectToThread({
     await context.storageState({ path: authStatePath })
     console.log('sendDirectToThread: Auth state saved')
   }
+  if (page.url() !== threadUrl) {
+    await page.goto(threadUrl)
+    console.log('sendDirectToThread: Thread opened')
+  }
   await page.getByRole('paragraph').fill(message)
   console.log('sendDirectToThread: Message filled')
+  await new Promise(resolve => setTimeout(resolve, 1_000))
   await page.getByRole('button', { name: /send/i }).click()
   console.log('sendDirectToThread: Send button pressed')
+  await new Promise(resolve => setTimeout(resolve, 2_000))
   await expect(page.getByRole('paragraph')).toHaveText('')
   await expect(page.getByLabel('Messages in conversation with')).toContainText(
     message,
   )
   console.log('sendDirectToThread: Message sent successfully')
   await browser.close()
+  console.log('sendDirectToThread: Browser closed')
 }
