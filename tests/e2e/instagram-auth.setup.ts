7c31e3888ac42a3ab154470f99fb31b5ceaeb92a
Amir Ziaei <amir.ziaei@zenitech.co.uk>
2024-03-27 20:24:46 +0200

    clean up

--- tests/e2e/instagram-auth.setup.ts
+++ tests/e2e/instagram-auth.setup.ts
@@ -2,24 +2,24 @@ import { test as setup, expect } from '@playwright/test'
 
 const authFile = 'mnt/auth.json'
 
+const noop = () => {}
+
 setup('authenticate instagram', async ({ page }) => {
   await page.goto('https://www.instagram.com/')
   await page
     .getByRole('button', { name: 'Decline optional cookies' })
     .click()
-    .catch(() => {})
+    .catch(noop)
   await page
     .getByLabel('Phone number, username, or')
     .fill(process.env.IG_USERNAME)
   await page.getByLabel('Password').fill(process.env.IG_PASSWORD)
   await page.getByRole('button', { name: 'Log in', exact: true }).click()
   await page.getByRole('button', { name: 'Save info' }).click()
-  try {
-    await expect(page.getByText('Turn on Notifications')).toBeVisible()
-    await page.getByRole('button', { name: 'Not Now' }).click()
-  } catch {
-    /** no need to handle */
-  }
+  await expect(page.getByText('Turn on Notifications'))
+    .toBeVisible()
+    .catch(noop)
+  await page.getByRole('button', { name: 'Not Now' }).click().catch(noop)
   await expect(page.getByRole('link', { name: 'Home Home' })).toBeVisible()
   await page.context().storageState({ path: authFile })
 })
