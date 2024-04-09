4215f1c85a241ddad9ef63f1e58289563ee28014
Amir Ziaei <amir.ziaei@zenitech.co.uk>
2024-04-09 13:57:04 +0300

    include user-agent for icanhazdadjoke per terms of service request

--- apps/app/src/services/jokes.ts
+++ apps/app/src/services/jokes.ts
@@ -1,6 +1,10 @@
 export async function getRandomDadJoke() {
   const response = await fetch('https://icanhazdadjoke.com/', {
-    headers: { Accept: 'text/plain' },
+    headers: {
+      Accept: 'text/plain',
+      'User-Agent':
+        'daily-dad-jokes-ig (https://github.com/amir-ziaei/daily-dad-jokes-ig)',
+    },
   })
   return (await response.text()).trim()
 }
