84a98b10795ca59ddb2a4e8f2a1ac4beae16f1b1
Amir Ziaei <amir.ziaei@zenitech.co.uk>
2024-04-09 13:52:48 +0300

    simplifying jokes service because icanhazdadjoke supports content negotiation - requesting text/plain instead of html

--- apps/app/src/services/jokes.ts
+++ apps/app/src/services/jokes.ts
@@ -1,8 +1,6 @@
-import * as cheerio from 'cheerio'
-
 export async function getRandomDadJoke() {
-  const response = await fetch('https://icanhazdadjoke.com/')
-  const html = await response.text()
-  const $ = cheerio.load(html)
-  return $('.card-content p.subtitle').text().trim()
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'text/plain' },
+  })
+  return (await response.text()).trim()
 }
